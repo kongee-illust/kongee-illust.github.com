11fb185bfc3a42ea6ad2935f8e9f1ba2e46c2652
Sally (Hyunji) Kim <shjkim@ucdavis.edu>
2021-11-22 11:53:36 -0800

    finished studio3 and fixed svg pathways

--- des157a/studio3/script.js
+++ des157a/studio3/script.js
@@ -5,15 +5,14 @@
     //your code starts here!
     const startGame = document.getElementById('startgame');
     const gameControl = document.getElementById('gamecontrol');
-    const game = document.getElementById('game');
-    const score = document.getElementById('score');
-    const actionArea = document.getElementById('actions');
 
     // sounds used for this project
     const diceRoll = new Audio("media/Shake_And_Roll_Dice.mp3")
+    const dingDongDeng = new Audio("media/사르르.MP3")
+    const startSound = new Audio("media/맞추었어.MP3")
 
     const gameData = {
-        dice: ["images/die-1.png", "images/die-2.png", "images/die-3.png", "images/die-4.png", "images/die-5.png", "images/die-6.png"],
+        dice: ["images/die-1.svg", "images/die-2.svg", "images/die-3.svg", "images/die-4.svg", "images/die-5.svg", "images/die-6.svg"],
         players: ['player 1', 'player 2'],
         score: [0, 0],
         roll1: 0,
@@ -22,15 +21,17 @@
         index: 0,
         gameEnd: 29
     }
+
+    // modified dom from pig.js
     const gameDom = {
         player1: document.querySelector('.player-card.one'),
         score1: document.querySelector('.player-card.one .score'),
-        dice11: document.querySelector('.player-card.one .dice.one'),
-        dice12: document.querySelector('.player-card.one .dice.two'),
+        dice11: document.querySelector('.player-card.one .dice-holder.dice1'),
+        dice12: document.querySelector('.player-card.one .dice-holder.dice2'),
         player2: document.querySelector('.player-card.two'),
         score2: document.querySelector('.player-card.two .score'),
-        dice21: document.querySelector('.player-card.two .dice.one'),
-        dice22: document.querySelector('.player-card.two .dice.two'),
+        dice21: document.querySelector('.player-card.two .dice-holder.dice1'),
+        dice22: document.querySelector('.player-card.two .dice-holder.dice2'),
         gameinfo: document.getElementById('game-info')
     }
 
@@ -52,44 +53,49 @@
         console.log("set up the turn!");
 
         setUpTurn();
+        //sound effect plays
+        startSound.play();
     })
+
+    // quitting game would reload the script
     document.getElementById("quit").addEventListener("click", function() {
         location.reload();
     })
     document.getElementById("roll").addEventListener("click", function() {
+        if (gameData.score[gameData.index] > gameData.gameEnd) return;
         console.log("roll the dice!");
-    })
+        //dice sound plays;
+        diceRoll.play();
 
-    // need to follow game flow
-    // fix old code for game info with new dom
-    // maybe add dice animation and time delays
+        throwDice();
+    })
+    document.getElementById("pass").addEventListener("click", function() {
+        if (gameData.score[gameData.index] > gameData.gameEnd) return;
+        gameData.index = gameData.index ? 0 : 1;
+        setUpTurn();
+    });
 
     function setUpTurn() {
         /* gameData player variable here! */
+        // gameData.index = gameData.index ? 0 : 1;
+
         setGameinfoText(`<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`)
-
-
-        //dice sound plays;
-        diceRoll.play;
-
-        throwDice();
-        checkWinningCondition();
 
     }
 
     function throwDice() {
-        actionArea.innerHTML = "";
         gameData.roll1 = Math.floor(Math.random() * 6) + 1; //using ceil could result in a zero!
         gameData.roll2 = Math.floor(Math.random() * 6) + 1;
-        game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
-        game.innerHTML += `<img src="${gameData.dice[gameData.roll1-1]}"> <img src="${gameData.dice[gameData.roll2-1]}">`;
+        gameDom[`dice${(gameData.index + 1)}1`].innerHTML = `<img src="${gameData.dice[gameData.roll1 - 1]}">`
+        gameDom[`dice${(gameData.index + 1)}2`].innerHTML = `<img src="${gameData.dice[gameData.roll2 - 1]}">`
+
         gameData.rollSum = gameData.roll1 + gameData.roll2;
 
         //if 2 1's are rolled
         if (gameData.rollSum === 2) {
             console.log("snake eyes were rolled!");
 
-            game.innerHTML += "<p>Oh snap! Snake eyes!</p>"
+            setGameinfoText("<p>Oh snap! Snake eyes!</p>")
             gameData.score[gameData.index] = 0;
             gameData.index ? (gameData.index = 0) : (gameData.index = 1);
             //show current score
@@ -100,7 +106,7 @@
             console.log("one of the two dice was a 1!")
 
             gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-            game.innerHTML += `<p>Sorry, one of your rolls was a one, switching to ${gameData.players[gameData.index]}</p>`
+            setGameinfoText(`<p>One of your rolls was a 1! Switching to ${gameData.players[gameData.index]}</p>`)
             setTimeout(setUpTurn, 2000);
         }
         //if neither is a 1
@@ -108,16 +114,7 @@
             console.log("the game proceeds...")
 
             gameData.score[gameData.index] += gameData.rollSum;
-            actionArea.innerHTML = '<button id="rollagain">Roll again</button> or <button id="pass">Pass</button>';
-
-            document.getElementById("rollagain").addEventListener("click", function() {
-                setUpTurn();
-            });
-
-            document.getElementById("pass").addEventListener("click", function() {
-                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-                setUpTurn();
-            });
+            gameDom['score' + (gameData.index + 1)].innerHTML = gameData.score[gameData.index];
 
             //check winning condition :D !
             checkWinningCondition();
@@ -126,19 +123,40 @@
 
     function checkWinningCondition() {
         if (gameData.score[gameData.index] > gameData.gameEnd) {
-            score.innerHTML = `<h2>${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</h2>`
-
-            actionArea.innerHTML = "";
-            document.getElementById("quit").innerHTML = "Start a New Game?";
+            setGameinfoText(`<h2>${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</h2>`)
+            document.getElementById("quit").innerHTML = "New Game";
+            // sound effect plays
+            dingDongDeng.play();
         } else {
             //show current score...
-            score.innerHTML = `<p>The score is currently <strong>${gameData.players[0]} ${gameData.score[0]}</strong> and <strong>${gameData.players[1]} ${gameData.score[1]}</strong></p>`;
+            setGameinfoText(`<p>The score is currently <strong>${gameData.players[0]} ${gameData.score[0]}</strong> and <strong>${gameData.players[1]} ${gameData.score[1]}</strong></p>`)
         }
 
         // checkWinningCondition();
     }
 
-    function showCurrentScore() {
-        score.innerHTML = `<p>The score is currently <strong>${gameData.players[0]} ${gameData.score[0]}</strong> and <strong>${gameData.players[1]} ${gameData.score[1]}</strong></p>`;
-    }
+
+    // edge case: clicking on the child of the overlay would close the overlay
+    // so stopPropagation prevents that
+    let overlayContent = document.querySelector("#overlay .content");
+    overlayContent.addEventListener('click', (event) => {
+        event.stopPropagation();
+    })
+
+    //clicking help button, dark overlay background and soundsgood button all toggle the overlay
+    let helpButton = document.getElementById("helpButton");
+    helpButton.addEventListener('click', () => {
+        overlayElem.classList.toggle("open");
+    })
+    let overlayElem = document.getElementById("overlay");
+    overlayElem.addEventListener('click', () => {
+        overlayElem.classList.toggle("open");
+    })
+    let soundsGoodButton = document.querySelector("#overlay .content button");
+    soundsGoodButton.addEventListener('click', () => {
+        overlayElem.classList.toggle("open");
+    })
+
+
+
 })();
