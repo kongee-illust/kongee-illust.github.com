cad92d35fcb8100d30256360848555c060cc9859
Sally iMac <Sally@iMac.local>
2022-03-08 02:05:41 -0800

    version3 and portal updated

--- des157b/final/development/version3/script.js
+++ des157b/final/development/version3/script.js
@@ -1,5 +1,6 @@
 (function() {
     "use strict";
+
     console.log("reading js");
     const menu = $('#menu')
     console.log(menu)
@@ -7,4 +8,69 @@
         menu[0].classList.toggle('open')
             // console.log(123)
     })
+
+    // window.alert('thank you for testing my product! please: 1. scroll through the landing page, 2. add your story using the file provided to you, and 3. view our stories! note: you may have to wait a few minutes for your submission to show up.')
+
+    function tsvToHtml(text) {
+        console.log(text)
+        let lines = text.split('\n')
+            // gets rid of first line which is labels
+        lines = lines.filter((val, index) => index)
+        lines = lines.map(line => line.split('	'))
+
+        let result = ''
+        lines.forEach((item, index) => {
+            // skip every second one
+            if (index % 2 === 1) return
+                // push item and next item to result
+            let grouped = [card(item)]
+            let next = lines[index + 1]
+            if (next) grouped.push(card(next))
+
+            result = result.concat(row(grouped))
+        });
+
+        return result;
+    }
+
+    function card(data) {
+
+        //some people may not want to input their names; the google form shows "optional" for the name field
+        let authorLine;
+        if (data[1]) {
+            authorLine = `Story told by ${data[1]}`
+        } else {
+            authorLine = 'Story told by Anonymous'
+        }
+
+        // changes url so it can be viewed in a website
+        let imageUrl = data[5] ? `./assets/${data[5]}` : data[3].replace('open', 'uc');
+
+        return `
+                <div class="cards">
+                    <img src="${imageUrl}">
+                    <div class="card-title-author">
+                    <h2>${data[2]}</h2>
+                    <p>${authorLine}</p>
+                    </div>
+                    <p id="card-description">${data[4]}</p>
+                </div>
+                `
+    }
+
+    function row(cards) {
+        return `
+        <div class=rows>
+            ${cards.join('')}
+        </div>
+        `
+    }
+
+    fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4wggNORhqrcvg48RyiXIt66i_HrJcPc46FFAQrkXo0Q9PEnTmIpq_jVnwg48_RvaDKf-TDuMlSv-4/pub?output=tsv")
+        .then(res => res.text())
+        .then(tsvToHtml)
+        .then(html => $('#viewstories').html(html))
+
+
+
 })();
